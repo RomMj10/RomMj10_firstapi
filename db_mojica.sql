16d6739ffaca7b32889c64d7b17879250267f073
CJonesOveHevun <jeromemojica10@gmail.com>
2026-01-17 18:03:43 +0800

    Added example sql file

---
+++ db_mojica.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:4308
+-- Generation Time: Jan 17, 2026 at 11:02 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_mojica`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_student`
+--
+
+CREATE TABLE `tbl_student` (
+  `id` int(10) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `gender` varchar(2) NOT NULL,
+  `age` int(10) NOT NULL,
+  `course_id` int(10) NOT NULL,
+  `department_id` int(10) NOT NULL,
+  `status` varchar(10) NOT NULL DEFAULT 'ACTIVE'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_student`
+--
+
+INSERT INTO `tbl_student` (`id`, `firstname`, `lastname`, `gender`, `age`, `course_id`, `department_id`, `status`) VALUES
+(1, 'Jerome', 'Mojica', 'M', 22, 1, 1, 'ACTIVE'),
+(2, 'Justine', 'Mojica', 'M', 22, 2, 2, 'ACTIVE'),
+(3, 'John', 'Doe', 'M', 28, 2, 1, 'INACTIVE'),
+(5, 'Emily', 'Heather', 'F', 28, 1, 1, 'ACTIVE'),
+(6, 'Harry', 'Osborn', 'M', 25, 1, 2, 'ACTIVE'),
+(7, 'Mary', 'Jane', 'F', 27, 3, 1, 'ACTIVE');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_student`
+--
+ALTER TABLE `tbl_student`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_student`
+--
+ALTER TABLE `tbl_student`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
